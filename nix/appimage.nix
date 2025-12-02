3c93f380b868c9539eaa4c8a8da788cdf3e9be8d
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-12-02 19:41:00 +0100

    use mkNixAppImageBundle from nixpkgs overlay

--- nix/appimage.nix
+++ nix/appimage.nix
@@ -1,20 +1,12 @@
 { callPackage
-, fetchFromGitHub
-, system
+, mkNixAppImageBundle
 , ...
 } @ args:
 
 let
-  nix-appimage = fetchFromGitHub {
-    owner = "ralismark";
-    repo = "nix-appimage";
-    rev = "5dadbcd9f0da9adc49d6d831d5c2eab0bbfdae67";
-    hash = "sha256-rSwndd+4JmGrjR/APDTgJlXTEPwfCmt7UF+SceW3bfM=";
-  };
-  bundler = (import nix-appimage).bundlers.${system}.default;
   fn = import ./.;
 in
-  bundler (
+  mkNixAppImageBundle (
     callPackage fn (builtins.intersectAttrs
       (builtins.functionArgs fn) ({ buildAppImage = true; } // args)
     )
